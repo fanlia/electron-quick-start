6c4fb8d5cdc37faff36a64c184aa7042f0ae154a
fanlia <you@example.com>
2022-10-13 20:55:30 +0800

    refine

--- preload.js
+++ preload.js
@@ -2,9 +2,13 @@
  * The preload script runs before. It has access to web APIs
  * as well as Electron's renderer process modules and some
  * polyfilled Node.js functions.
- * 
+ *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
+
+var usbDetect = require('usb-detection');
+usbDetect.startMonitoring();
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -14,4 +18,15 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  const usb = document.getElementById('usb')
+  usb.textContent = 'how many usbs now?'
+
+  usbDetect.find((err, devices) => {
+      if (err) {
+          usb.textContent = 'error: ' + err.message
+      } else {
+          usb.textContent = 'ok: ' + devices.length
+      }
+  })
 })
